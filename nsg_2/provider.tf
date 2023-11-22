5d47c7b3ea37463666d8b980d89b35bfd29cc035
Anand Rao <anand.rao@wpp.com>
2023-11-22 07:30:00 +0530

    a

--- nsg_2/provider.tf
+++ nsg_2/provider.tf
@@ -34,18 +34,18 @@ terraform {
 }
 
 
-# provider "azurerm" {
-#   subscription_id = "6e54aa79-9b0b-40ed-ae27-91fdffa0a565"
-#   tenant_id       = "17a09c66-f406-4897-8b24-51f5313623dc"
-#   client_id       = "6589b09f-b35e-4dd2-86fc-d7a757d9f72e"
-#   client_secret   = "uEt8Q~bOJthtZLylI39GkKNrov7scwN2uf1MoaLU"
-#   features {
-#   }
+provider "azurerm" {
+  # subscription_id = "6e54aa79-9b0b-40ed-ae27-91fdffa0a565"
+  # tenant_id       = "17a09c66-f406-4897-8b24-51f5313623dc"
+  # client_id       = "6589b09f-b35e-4dd2-86fc-d7a757d9f72e"
+  # client_secret   = "uEt8Q~bOJthtZLylI39GkKNrov7scwN2uf1MoaLU"
+  features {
+  }
 
 
 
 
-# }
+}
 
 # provider "azapi" {
 #   subscription_id = "6e54aa79-9b0b-40ed-ae27-91fdffa0a565"
